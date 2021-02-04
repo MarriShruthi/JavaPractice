596c83f7abd97530d33213665af2c54c1958e067
Shruthi <shruthimarri97@gmail.com>
2021-02-04 15:43:52 +0530

    Welcome to line computation

---
+++ Line.java
@@ -0,0 +1,12 @@
+public class Line
+{
+	public static int line()	
+	{
+	   System.out.println("Welcome to Line Comparison Computation");
+	    return 0; 
+	}
+	public static void main(String args[])
+	{
+		line(); 
+	}
+}
